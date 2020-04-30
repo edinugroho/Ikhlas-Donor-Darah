aafd9fa975ac2bea3aaad8967a49466823af27a7
edinugroho <edidwinugroho80@gmail.com>
2020-04-30 13:47:21 +0700

    fix view

--- application/views/carousel.php
+++ application/views/carousel.php
@@ -11,8 +11,8 @@
     <li>
       <img src="https://images.unsplash.com/photo-1495653797063-114787b77b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"> <!-- random image -->
       <div class="caption center-align">
-        <h3>Ikhlas Donor</h3>
-        <h5 class="light grey-text text-lighten-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quaerat ab magnam, error, ipsam, sequi explicabo blanditiis qui sunt perspiciatis quasi reprehenderit officia laborum, natus ullam iusto eaque nemo impedit.</h5>
+        <h3 class="black-text text-darken-2">Ikhlas Donor</h3>
+        <h5 class="black-text text-darken-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quaerat ab magnam, error, ipsam, sequi explicabo blanditiis qui sunt perspiciatis quasi reprehenderit officia laborum, natus ullam iusto eaque nemo impedit.</h5>
       </div>
     </li>
     <li>
@@ -25,8 +25,8 @@
     <li>
       <img src="https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"> <!-- random image -->
       <div class="caption right-align">
-        <h3>Klik Mulai Untuk menggunakan Fitur</h3>
-        <h5 class="light grey-text text-lighten-3">Mulai Aja dulu hehe</h5>
+        <h3 class="teal-text text-darken-2">Klik Mulai Untuk menggunakan Fitur</h3>
+        <h5 class="light black-text text-lighten-3">Mulai Aja dulu</h5>
         <a href="<?php echo base_url('welcome/home') ?>" class="btn waves-effect waves-light" type="submit" name="action">Mulai</a>
       </div>
     </li>
